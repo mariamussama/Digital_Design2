a85a02986184d58efcd7b4c92150c4edf6147c82
mariamussama <78415993+mariamussama@users.noreply.github.com>
2022-12-11 18:14:39 +0200

    more optimization

--- src/Source.py
+++ src/Source.py
@@ -1,7 +1,5 @@
 import random
 import math
-import numpy as np
-import copy
 
 
 def HPWL(netlist, cell, r, c, n_nets):
@@ -9,24 +7,13 @@ def HPWL(netlist, cell, r, c, n_nets):
     init_net = {}
     k = 0
     for net in netlist:
-        l_max = 0
-        l_min = r
-        w_max = 0
-        w_min = c
+        X = []
+        Y = []
         for i in net:
-            if cell[int(i)][0] > l_max:
-                l_max = cell[int(i)][0]
-            elif cell[int(i)][0] < l_min:
-                l_min = cell[int(i)][0]
-            if cell[int(i)][1] > w_max:
-                w_max = cell[int(i)][1]
-            elif cell[int(i)][1] < w_min:
-                w_min = cell[int(i)][1]
-        # print("lmax, lmin", l_max, l_min)
-        # print("wmax, wmin", w_max, w_min)
-        halfPara = abs(l_max - l_min) + abs(w_max - w_min)
+            X.append(cell[i][0])
+            Y.append(cell[i][1])
+        halfPara = (max(X)-min(X))+(max(Y)-min(Y))
         init_net[k] = halfPara
-        # print("hpwl small= ", halfPara)
         total = total + halfPara
         # print(k)
         k = k + 1
@@ -39,157 +26,52 @@ def mapping_cells(netlist, N):
     mapp = [[] for j in range(N)]
     for net in netlist:
         for i in net:
-            mapp[int(i)].append(count)
+            mapp[i].append(count)
         count = count + 1
     return mapp
 
 
-def HPWL_mod(init_NET, initial_HPWL, netlist, cel, mapping, c1, c2, r, c):
-    # mod = []
-    # if c1 == -1:
-    #     mod.append(mapping[c2])
-    # elif c2 == -1:
-    #     mod.append(mapping[c1])
-    # else:
-    #     mod.append(mapping[c1])
-    #
-    # if len(mod) != 0:
-    #     loop = 0
-    #     for j in mod:
-    #         l_max = 0
-    #         l_min = r
-    #         w_max = 0
-    #         w_min = c
-    #         if loop != 0:
-    #             # print(i)
-    #             loop2 = 0
-    #             for i in netlist[j].split():
-    #                 if loop2 != 0:
-    #                     if cel[int(i)][0] > l_max:
-    #                         l_max = cel[int(i)][0]
-    #                     if cel[int(i)][0] < l_min:
-    #                         l_min = cel[int(i)][0]
-    #                     if cel[int(i)][1] > w_max:
-    #                         w_max = cel[int(i)][1]
-    #                     if cel[int(i)][1] < w_min:
-    #                         w_min = cel[int(i)][1]
-    #                 loop2 = loop2 + 1
-    #             # print(c1)
-    #             # print("lmax, lmin", l_max, l_min)
-    #             # print("wmax, wmin", w_max, w_min)
-    #             halfPara = abs(l_max - l_min) + abs(w_max - w_min)
-    #             # print(initial_HPWL, init_NET[j], halfPara)
-    #             initial_HPWL = initial_HPWL - init_NET[j]
-    #             init_NET[j] = halfPara
-    #             initial_HPWL = initial_HPWL + halfPara
-    #         loop = loop + 1
-    #
-    #     if c1 != -1 and c2 != -1:
-    #         for j in mapping[c2]:
-    #             if j not in mod:
-    #                 l_max = 0
-    #                 l_min = r
-    #                 w_max = 0
-    #                 w_min = c
-    #                 if loop != 0:
-    #                     # print(i)
-    #                     loop2 = 0
-    #                     for i in netlist[j].split():
-    #                         if loop2 != 0:
-    #                             if cel[int(i)][0] > l_max:
-    #                                 l_max = cel[int(i)][0]
-    #                             if cel[int(i)][0] < l_min:
-    #                                 l_min = cel[int(i)][0]
-    #                             if cel[int(i)][1] > w_max:
-    #                                 w_max = cel[int(i)][1]
-    #                             if cel[int(i)][1] < w_min:
-    #                                 w_min = cel[int(i)][1]
-    #                         loop2 = loop2 + 1
-    #                     # print(c1)
-    #                     # print("lmax, lmin", l_max, l_min)
-    #                     # print("wmax, wmin", w_max, w_min)
-    #                     halfPara = abs(l_max - l_min) + abs(w_max - w_min)
-    #                     # print(initial_HPWL, init_NET[j], halfPara)
-    #                     initial_HPWL = initial_HPWL - init_NET[j]
-    #                     init_NET[j] = halfPara
-    #                     initial_HPWL = initial_HPWL + halfPara
-    if c1 != -1:
-        for j in mapping[c1]:
-            l_max = 0
-            l_min = r
-            w_max = 0
-            w_min = c
+def HPWL_mod_2(init_NET, initial_HPWL, netlist, cel, mapping, c1, c2):
+
+    for j in mapping[c1]:
+        X = []
+        Y = []
+        for i in netlist[j]:
+            X.append(cel[i][0])
+            Y.append(cel[i][1])
+        halfPara = (max(X)-min(X))+(max(Y)-min(Y))
+        # print(initial_HPWL, init_NET[j], halfPara)
+        initial_HPWL = initial_HPWL - init_NET[j] + halfPara
+        init_NET[j] = halfPara
+        # initial_HPWL = initial_HPWL + halfPara
+
+    for j in mapping[c2]:
+        if j not in mapping[c1]:
+            X = []
+            Y = []
             for i in netlist[j]:
-                if cel[int(i)][0] > l_max:
-                    l_max = cel[int(i)][0]
-                elif cel[int(i)][0] < l_min:
-                    l_min = cel[int(i)][0]
-                if cel[int(i)][1] > w_max:
-                    w_max = cel[int(i)][1]
-                elif cel[int(i)][1] < w_min:
-                    w_min = cel[int(i)][1]
-            # print(c1)
-            # print("lmax, lmin", l_max, l_min)
-            # print("wmax, wmin", w_max, w_min)
-            halfPara = abs(l_max - l_min) + abs(w_max - w_min)
+                X.append(cel[i][0])
+                Y.append(cel[i][1])
+            halfPara = (max(X)-min(X))+(max(Y)-min(Y))
             # print(initial_HPWL, init_NET[j], halfPara)
-            initial_HPWL = initial_HPWL - init_NET[j]
+            initial_HPWL = initial_HPWL - init_NET[j] + halfPara
             init_NET[j] = halfPara
-            initial_HPWL = initial_HPWL + halfPara
-    if c2 != -1:
-        for j in mapping[c2]:
-            if j not in mapping[c1]:
-                # mod.append(i)
-                l_max = 0
-                l_min = r
-                w_max = 0
-                w_min = c
-                for i in netlist[j]:
-                    if cel[int(i)][0] > l_max:
-                        l_max = cel[int(i)][0]
-                    elif cel[int(i)][0] < l_min:
-                        l_min = cel[int(i)][0]
-                    if cel[int(i)][1] > w_max:
-                        w_max = cel[int(i)][1]
-                    elif cel[int(i)][1] < w_min:
-                        w_min = cel[int(i)][1]
-                halfPara = abs(l_max - l_min) + abs(w_max - w_min)
-                # print(initial_HPWL, init_NET[j], halfPara)
-                initial_HPWL = initial_HPWL - init_NET[j]
-                init_NET[j] = halfPara
-                initial_HPWL = initial_HPWL + halfPara
-    # else:
-    # if len(mod) != 0:
-    #     loop = 0
-    #     for j in mod:
-    #         l_max = 0
-    #         l_min = r
-    #         w_max = 0
-    #         w_min = c
-    #         if loop != 0:
-    #             # print(i)
-    #             loop2 = 0
-    #             for i in netlist[j].split():
-    #                 if loop2 != 0:
-    #                     if cel[int(i)][0] > l_max:
-    #                         l_max = cel[int(i)][0]
-    #                     if cel[int(i)][0] < l_min:
-    #                         l_min = cel[int(i)][0]
-    #                     if cel[int(i)][1] > w_max:
-    #                         w_max = cel[int(i)][1]
-    #                     if cel[int(i)][1] < w_min:
-    #                         w_min = cel[int(i)][1]
-    #                 loop2 = loop2 + 1
-    #             # print(c1)
-    #             # print("lmax, lmin", l_max, l_min)
-    #             # print("wmax, wmin", w_max, w_min)
-    #             halfPara = abs(l_max - l_min) + abs(w_max - w_min)
-    #             # print(initial_HPWL, init_NET[j], halfPara)
-    #             initial_HPWL = initial_HPWL - init_NET[j]
-    #             init_NET[j] = halfPara
-    #             initial_HPWL = initial_HPWL + halfPara
-    #         loop = loop + 1
-    # return init_NET, initial_HPWL
+            # initial_HPWL = initial_HPWL + halfPara
+
+    return init_NET, initial_HPWL
+
+
+def HPWL_mod_1(init_NET, initial_HPWL, netlist, cel, mapping, c):
+    for j in mapping[c]:
+        X = []
+        Y = []
+        for i in netlist[j]:
+            X.append(cel[i][0])
+            Y.append(cel[i][1])
+        halfPara = (max(X)-min(X))+(max(Y)-min(Y))
+        # print(initial_HPWL, init_NET[j], halfPara)
+        initial_HPWL = initial_HPWL - init_NET[j] + halfPara
+        init_NET[j] = halfPara
 
     return init_NET, initial_HPWL
 
@@ -202,10 +84,6 @@ def readfile(path):
         print(N[0])
         cells = {}
         rows, cols = (int(N[2]), int(N[3]))
-        c = [[-1 for i in range(cols)] for j in range(rows)]
-
-        # cells = [[0 for i in range(2)] for j in range(int(N[0]))]
-        # core = np.array(c)
         core = {}
         for row in range(0, rows, 1):
             for col in range(0, cols, 1):
@@ -229,6 +107,7 @@ def readfile(path):
         for line in file:
             line = line.split()
             line.pop(0)
+            line = [eval(i) for i in line]
             # print(line)
             netlist.append(line)
         mapping = mapping_cells(netlist, int(N[0]))
@@ -311,20 +190,9 @@ def equation(change_length, tem):
 
 
 def update(cell, index, y, x):
-    # array2 = [[0 for i in range(2)] for j in range(n)]
-    # for row in range(0, n, 1):
-    #     for col in range(0, 2, 1):
-    #         array2[row][col] = best_cells[row][col]
-    # array2 = best_cells.copy()
-    # array2[index][0] = x_2
-    # array2[index][1] = y_2
-    # print(cell[index][0], cell[index][1])
-    # cel2 = cell.copy()
-    # cel2 = np.array(cell)
     cell[index] = (x, y)
-    # print(cell[index][0], cell[index][1])
-    # print(cel2[index][0], cel2[index][1])
-    return cell #, cel2
+
+    return cell
 
 
 # create annealing algorithm
@@ -339,7 +207,7 @@ def annealing(current_core, cell, rows, cols, N, netlist, n_nets, mapping):
     print(HPWL_initial)
     T = 500 * HPWL_initial
     # set cooling rate
-    T_final = (5 * 0.000001 * HPWL_initial) / n_nets
+    T_final = (5 * 0.00001 * HPWL_initial) / n_nets
 
     moves_per_temp = 10 * N
     HPWL_before = HPWL_initial
@@ -351,7 +219,7 @@ def annealing(current_core, cell, rows, cols, N, netlist, n_nets, mapping):
             y_1 = random.randint(0, rows - 1)
             y_2 = random.randint(0, rows - 1)
 
-            while (x_1 == x_2) and (y_1 == y_2):
+            while ((x_1 == x_2) and (y_1 == y_2)) or (best_core[y_1, x_1] == -1 and best_core[y_2, x_2] == -1):
                 # choose another cell for cell_2
                 x_2 = random.randint(0, cols - 1)
                 y_2 = random.randint(0, rows - 1)
@@ -360,35 +228,34 @@ def annealing(current_core, cell, rows, cols, N, netlist, n_nets, mapping):
             # swaps 2 current_cells together
             # new_core = swap_core(best_core.copy(), x_1, x_2, y_1, y_2, rows, cols)
             # print("new")
-            new_cells = best_cells.copy()
+            # new_hpwl = HPWL_before
+            # new_init_net = {}
             if (best_core[y_2, x_2] != -1) and (best_core[y_1, x_1] != -1):
+                new_cells = best_cells.copy()
                 temp = new_cells[best_core[y_1, x_1]]
                 new_cells[best_core[y_1, x_1]] = new_cells[best_core[y_2, x_2]]
                 new_cells[best_core[y_2, x_2]] = temp
-                # new_cells = swap(best_cells.copy(), , best_core[y_2, x_2])
-                # print("1")
+                new_init_net, new_hpwl = HPWL_mod_2 (init_net.copy(), HPWL_before, netlist, new_cells.copy(), mapping,
+                         best_core[y_1, x_1], best_core[y_2, x_2])
             elif best_core[y_1, x_1] != -1:
                 # new_cells = update(best_cells.copy(), best_core[y_1, x_1], y_2, x_2)
+                new_cells = best_cells.copy()
                 new_cells[best_core[y_1, x_1]] = (x_2, y_2)
+                new_init_net, new_hpwl = HPWL_mod_1(init_net.copy(), HPWL_before, netlist, new_cells.copy(), mapping,
+                                                    best_core[y_1, x_1])
                 # print("2")
             elif best_core[y_2, x_2] != -1:
                 # new_cells = update(best_cells.copy(), best_core[y_2, x_2], y_1, x_1)
+                new_cells = best_cells.copy()
                 new_cells[best_core[y_2, x_2]] = (x_1, y_1)
+                new_init_net, new_hpwl = HPWL_mod_1(init_net.copy(), HPWL_before, netlist, new_cells.copy(), mapping,
+                                                    best_core[y_2, x_2])
                 # print("3")
 
             # calculate wire length using HPWL
-            new_init_net, new_hpwl = HPWL_mod(init_net.copy(), HPWL_before, netlist, new_cells.copy(), mapping,
-                                              best_core[y_1, x_1], best_core[y_2, x_2], rows, cols)
-            # print(best_core[y_1][x_1], best_core[y_2][x_2])
-            # print("x1", x_1, "y1", y_1, "x2", x_2, "y2", y_2)
-            # print(best_cells)
-            # print(best_core)
-            # print(new_cells)
-            # print(new_core)
-            # print(init_net)
-            # print(HPWL_before)
-            # print(new_init_net)
-            # print(new_hpwl)
+            # new_init_net, new_hpwl = HPWL_mod(init_net.copy(), HPWL_before, netlist, new_cells.copy(), mapping,
+            #                                   best_core[y_1, x_1], best_core[y_2, x_2], rows, cols)
+
             change_length = new_hpwl - HPWL_before
 
             if change_length < 0:
@@ -400,7 +267,7 @@ def annealing(current_core, cell, rows, cols, N, netlist, n_nets, mapping):
                 temp = best_core[y_2, x_2]
                 best_core[y_2, x_2] = best_core[y_1, x_1]
                 best_core[y_1, x_1] = temp
-                #best_core = new_core
+                # best_core = new_core
                 best_cells = new_cells
                 HPWL_before = new_hpwl
                 init_net = new_init_net.copy()
@@ -436,7 +303,7 @@ def annealing(current_core, cell, rows, cols, N, netlist, n_nets, mapping):
     return HPWL_before, best_core, best_cells
 
 
-mapping, core, cells, rows, cols, N, netlist, n_nets = readfile("t1.txt")
+mapping, core, cells, rows, cols, N, netlist, n_nets = readfile("t3.txt")
 print(netlist)
 print_core(core, rows, cols)
 print()
@@ -445,4 +312,3 @@ print_core(core2, rows, cols)
 print()
 print("Total wire length = ", end=" ")
 print(final_hpwl)
-
